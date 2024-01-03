443950a24a1b7f96babc9a1c76c170117575f214
Lucas Hoffmann <lucashoffmann99@gmail.com>
2024-01-02 21:43:42 -0300

    Added NavBar

---
+++ src/components/ProductCard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  StackDivider,
+  Heading,
+  Stack,
+  Box,
+} from "@chakra-ui/react";
+// import { Container } from './styles';
+
+function ProductCard() {
+  return (
+    <Card width="xs">
+      <CardHeader>
+        <Heading size="md">Client Report</Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Summary
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              View a summary of all your clients over the last month.
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Overview
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              Check out the overview of your clients.
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Analysis
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              See a detailed analysis of all your business clients.
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default ProductCard;
